87ee1487a6e98811b2a8468ed99d0e8adfefeaff
aidooyaw <aidooyaw1992@gmail.com>
2023-11-08 14:04:50 +0000

    added biometric page ui

--- app/src/main/java/com/faabsystems/pensionapplication/UserBiometricRegistrationFragment.java
+++ app/src/main/java/com/faabsystems/pensionapplication/UserBiometricRegistrationFragment.java
@@ -2,18 +2,32 @@ package com.faabsystems.pensionapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+import com.faabsystems.pensionapplication.databinding.FragmentUserBiometricRegistrationBinding;
+
 public class UserBiometricRegistrationFragment extends Fragment {
 
+    private FragmentUserBiometricRegistrationBinding fragmentUserBiometricRegistrationBinding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_biometric_registration, container, false);
+
+
+        fragmentUserBiometricRegistrationBinding = FragmentUserBiometricRegistrationBinding.inflate(inflater);
+        return fragmentUserBiometricRegistrationBinding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
